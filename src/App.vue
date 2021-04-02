ba8b389f9eec5a387d9895d351d993c8c45816fc
sorankh <khaledisoran1999@gmail.com>
2021-04-02 18:53:28 +0430

    created header with change style on scroll

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-header></app-header>
+    <app-main class="main"></app-main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderVue from "./components/Header.vue";
+import mainVue from "./components/main.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    appHeader: HeaderVue,
+    appMain: mainVue,
+  },
+  methods: {
+    scroll() {
+      const header = document.querySelector("header");
+      const section1 = document.getElementsByClassName("section1")[0];
+
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entery => {
+          if(!entery.isIntersecting) {
+            header.classList.add('dark');
+          } else {
+            header.classList.remove('dark');
+          }
+        })
+      }, {
+        rootMargin: "-120px 0px 0px 0px"
+      });
+      observer.observe(section1);
+
+
+      const observer2 = new IntersectionObserver((entries) => {
+        entries.forEach(entery => {
+          if(!entery.isIntersecting) {
+            header.classList.add('scrolled');
+          } else{
+            header.classList.remove('scrolled');
+          }
+        })
+      }, {
+        threshold: 1
+      });
+
+      observer2.observe(section1);
+
+    },
+  },
+  mounted() {
+    this.scroll();
+  },
+};
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
+  .main {
+  }
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,127 @@
+<template>
+  <header>
+    <div class="logo">
+      <p>MyShop</p>
+    </div>
+    <nav class="nav">
+      <ul class="navbar">
+        <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Pricing</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">About Us</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">More</a></li>
+      </ul>
+
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  --color: #f3f3f3;
+  --color-inverse: #333;
+  --background: transparent;
+  --size: 1rem;
+  width: 100%;
+  margin: 0;
+  padding: 30px 0;
+  position: fixed;
+  background-color: var(--background);
+  color: var(--color);
+  font-family: "Courier New", Courier, monospace;
+  font-size: 18px;
+  display: grid;
+  grid-template-columns: 1fr 5fr;
+  transition: all 0.8s cubic-bezier(0, 0.5, 0, 1);
+
+    .logo {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+
+        p {
+            margin: 0 20px;
+            text-align: center;
+            font-weight: 200;
+            font-size: 22px;
+        }
+    }
+  nav {
+    margin: 0 auto;
+    width: 100%;
+    display: grid;
+    justify-content: flex-end;
+
+    .navbar {
+      margin: 0 auto;
+      list-style: none;
+            padding: 15px 0;
+
+      .nav-item {
+        .nav-link {
+          color: var(--color);
+                    margin-left: 10px;
+                    padding: 5px 10px;
+                    transition: all 0.25s ease-in-out;
+        }
+      }
+    }
+  }
+}
+
+.scrolled {
+    background-color: #333;
+    color: #f4f4f4;
+    padding: 0;
+    box-shadow: 0px 1px 10px rgba(0, 0, 0, 0.2);
+
+    nav {
+        .navbar {
+            padding: 15px 0;
+            .nav-item {
+                .nav-link {
+                    margin-left: 10px;
+                    padding: 5px 10px;
+                    color: #f4f4f4;
+
+                    &:hover {
+                        background-color: lightgray;
+                        color: #333;
+                    }
+                }
+            }
+        }
+    }
+}
+
+.dark {
+    background-color: #f4f4f4;
+    color: #333;
+    padding: 0;
+    box-shadow: 0px 1px 10px rgba(0, 0, 0, 0.2);
+
+    nav {
+        .navbar {
+            padding: 15px 0;
+            .nav-item {
+                .nav-link {
+                    margin-left: 10px;
+                    padding: 5px 10px;
+                    color: #333;
+                    transition: all 0.25s ease-in-out;
+
+                    &:hover {
+                        background-color: lightgray;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+</style>
